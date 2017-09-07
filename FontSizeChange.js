a5eb4771cf83c1cd15c7796ef9a4e76b5eccc9ff
Justin Ernst <justin@Justins-MacBook-Pro.local>
2017-09-07 21:05:18 +0200

    Further changes and addition of jquery and ui/color plugins for animations

--- FontSizeChange.js
+++ FontSizeChange.js
@@ -1,7 +1,32 @@
-
-while(0 == 0) {
-    for(var i = 15; i < 50; i++){
-      document.getElementById("demo").style.fontSize = i + "px";
-      console.log(i);
-    }
-}
+$( "#demo").mouseenter(function() {
+  $(this).stop().animate({
+    fontSize: "42",
+    color: "#ff0000"
+  }, 200, 'swing', function() {
+    // Animation complete.
+  });
+});
+$( "#demo").mouseleave(function() {
+  $(this).stop().animate({
+    fontSize: "32",
+    color: "#0000ff"
+  }, 200, 'swing', function() {
+    // Animation complete.
+  });
+});
+$( "#clickme3" ).click(function() {
+  $( "#memes" ).stop().animate({
+    fontSize: "+=40",
+    color: "#ff0000"
+  }, 2000, 'easeOutBounce', function() {
+    // Animation complete.
+  });
+});
+$( "#clickme4" ).click(function() {
+  $( "#memes" ).stop().animate({
+    fontSize: "-=40",
+    color: "#0000ff"
+  }, 2000, 'easeOutBounce', function() {
+    // Animation complete.
+  });
+});
